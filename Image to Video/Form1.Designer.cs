6ed880c414a310e512bf4581a0c2f059c1ab6858
abelzis <abelzis@hotmail.com>
2019-09-16 17:57:59 +0300

    Add project files.

---
+++ Image to Video/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Image_to_Video
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.imageListPanel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imgListBox = new System.Windows.Forms.ListBox();
+            this.uploadText = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.imageListPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageListPanel
+            // 
+            this.imageListPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.imageListPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.imageListPanel.Controls.Add(this.imgListBox);
+            this.imageListPanel.Location = new System.Drawing.Point(12, 93);
+            this.imageListPanel.Name = "imageListPanel";
+            this.imageListPanel.Size = new System.Drawing.Size(470, 492);
+            this.imageListPanel.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1247, 28);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(46, 24);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.OpenToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(128, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // imgListBox
+            // 
+            this.imgListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imgListBox.FormattingEnabled = true;
+            this.imgListBox.ItemHeight = 16;
+            this.imgListBox.Location = new System.Drawing.Point(0, 0);
+            this.imgListBox.Name = "imgListBox";
+            this.imgListBox.Size = new System.Drawing.Size(470, 492);
+            this.imgListBox.TabIndex = 1;
+            this.imgListBox.SelectedIndexChanged += new System.EventHandler(this.ListBox1_SelectedIndexChanged);
+            // 
+            // uploadText
+            // 
+            this.uploadText.Location = new System.Drawing.Point(12, 50);
+            this.uploadText.Name = "uploadText";
+            this.uploadText.ReadOnly = true;
+            this.uploadText.Size = new System.Drawing.Size(1009, 22);
+            this.uploadText.TabIndex = 2;
+            this.uploadText.TabStop = false;
+            this.uploadText.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(1027, 50);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 3;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(1108, 50);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 4;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1247, 527);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.uploadText);
+            this.Controls.Add(this.imageListPanel);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.imageListPanel.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel imageListPanel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ListBox imgListBox;
+        private System.Windows.Forms.TextBox uploadText;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnUpload;
+    }
+}
+
---
+++ Image to Video/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+
+namespace Image_to_Video
+{
+    public partial class Form1 : Form
+    {
+        OpenFileDialog ofd;
+        List<Image<Rgb, Byte>> imageList;
+
+        public Form1()
+        {
+            InitializeComponent();
+            imageList = new List<Image<Rgb, byte>>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        // add image to image list vector
+        private void addImageToImageList(String filePath) { imageList.Add(new Image<Rgb, byte>(filePath)); }
+
+        // add image to listbox
+        private void addImageToListBox(String filePath) { imgListBox.Items.Add(filePath.Substring(filePath.LastIndexOf('\\') + 1)); }
+
+        private void addImage(String filePath)
+        {
+            addImageToImageList(filePath);
+            addImageToListBox(filePath);
+        }
+        
+        //insert images
+        private void OpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog ofd1 = new OpenFileDialog();
+                ofd1.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+                ofd1.Multiselect = true;
+
+                if (ofd1.ShowDialog() == DialogResult.OK)    //show the dialog, and if result is ok, upload the image
+                    foreach(String filePath in ofd1.FileNames)
+                        addImage(filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //browse button
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //on click, open file dialog
+                ofd = new OpenFileDialog();
+                ofd.Filter = "Image Files(*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|All files (*.*)|*.*";
+                ofd.Multiselect = true;
+
+                if (ofd.ShowDialog() == DialogResult.OK)    //show the dialog, and if result is ok, upload the image
+                {
+                    //if a single file is selected
+                    if (ofd.FileNames.GetLength(0) == 1)
+                        uploadText.Text = ofd.FileNames[0]; //set text in textbox
+                    //if multiple files are selected
+                    if (ofd.FileNames.GetLength(0) > 1)
+                    {
+                        // set text in textbox
+                        int last_index = ofd.FileNames.GetLength(0) - 1;
+                        String path = ofd.FileNames[0].Substring(0, ofd.FileNames[0].LastIndexOf('\\') + 1);
+                        String first_file = ofd.FileNames[0].Substring(ofd.FileNames[0].LastIndexOf('\\') + 1);
+                        String last_file = ofd.FileNames[last_index].Substring(ofd.FileNames[last_index].LastIndexOf('\\') + 1);
+                        uploadText.Text = path + "\"" + first_file + "\"...\"" + last_file + "\"";
+                    }
+                        
+                    //update textbox of the file path
+                    //uploadText.Text = ofd1.FileName;
+
+                    //uploadText.Text = uploadText.Text.Remove(uploadText.Text.LastIndexOf('\\'));
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        // Upload button
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (uploadText.Text != String.Empty)
+            {
+                try
+                {
+                    foreach (String filePath in ofd.FileNames)
+                        addImage(filePath);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+    }
+}
